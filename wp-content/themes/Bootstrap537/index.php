e5779e209bff636a22be8f1357f31e4efd70e1d1
Alejandro Lodes <janosoft@gmail.com>
2025-06-23 19:29:05 -0300

    Bootstrap 537

---
+++ wp-content/themes/Bootstrap537/index.php
@@ -0,0 +1,9 @@
+<?php include_once("header.php"); ?>
+<?php include_once("menu.php"); ?>
+<!-- Contenido -->
+<div class="container py-3">
+    <?php include_once("centrales.php"); ?>
+    <?php include_once("noticias.php"); ?>
+</div>
+<!-- END : Contenido -->
+<?php include_once("footer.php"); ?>
