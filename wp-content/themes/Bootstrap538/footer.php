35ab76851e1eab0f427cbe285e8e85aac4d6ac37
Alejandro Lodes <janosoft@gmail.com>
2025-08-31 12:32:49 -0300

    evita problemas con Rocket Loader

--- wp-content/themes/Bootstrap538/footer.php
+++ wp-content/themes/Bootstrap538/footer.php
@@ -20,8 +20,8 @@
 <?php wp_footer(); ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/gamertotal.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" data-cfasync="false"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/gamertotal.js" data-cfasync="false"></script>
 </body>
 
 </html>
